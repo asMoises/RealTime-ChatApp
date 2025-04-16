84ab190b309aed8f5019ac89be922c9ee94216fb
Moisés Silva <moiss.silva@outlook.com>
2025-04-16 13:07:36 -0300

    Criando a página de edição do profile

--- a_users/urls.py
+++ a_users/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
   path('', profile_view, name='profile'),  # Profile page URL
+  path('edit/', profile_edit_view, name='profile-edit'),  # Profile edit page URL
 ]
--- a_users/views.py
+++ a_users/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import *
 
 # Create your views here.
 def profile_view(request):
   profile = request.user.profile
   return render(request, 'a_users/profile.html', {'profile': profile})
+
+
+@login_required
+def profile_edit_view(request):
+  form = ProfileForm(instance=request.user.profile)
+  return render(request, 'a_users/profile_edit.html', {'form': form})
 
